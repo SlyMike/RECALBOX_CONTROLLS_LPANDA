602d78f00b8d5f4393f8728ad4d94021e5a8c6af
Mike Dalton <mikdalton@googlemail.com>
2019-07-25 14:55:58 +0100

    Added Arduino Leonardo files.
    Download files, open with Arduino IDE, upload to board, make interface, play games!

---
+++ RBLPControls.h
@@ -0,0 +1,107 @@
+#include <arduino.h>
+#include <Keyboard.h>
+
+#define BTN_P1_START		2	//			KEY_1
+#define BTN_P1_SELECT		3	//			KEY_3
+#define BTN_P1_UP			  4	//			KEY_UP
+#define BTN_P1_DOWN			5	//			KEY_DOWN
+#define BTN_P1_LEFT			6	//			KEY_LEFT
+#define BTN_P1_RIGHT		7	//			KEY_RIGHT
+#define BTN_P1_A			  8	//			KEY_Z
+#define BTN_P1_B			  9	//			LEFT_SHIFT
+#define BTN_P1_X			  10	//		LEFT_ALT
+#define BTN_P1_Y			  11	//		LEFT_CTRL
+#define BTN_P1_L1			  12	//		KEY_SPACE
+#define BTN_P1_R1			  A0  //		KEY_X
+#define BTN_FE_HOTHEY		A1  //		KEY_C
+#define BTN_FE_ACCEPT		A2  //		KEY_A
+#define BTN_FE_BACK			A3  //		KEY_S
+#define BTN_FE_MENU			A4  //		KEY_RETURN
+#define BTN_FE_QUITEM		A5  //		KEY_ESC
+
+
+typedef struct
+{
+  uint8_t state; // what state was the input last in ( HIGH/LOW )
+  unsigned long last_change;
+} button_state;
+
+
+/*
+#define KEY_LEFT_CTRL			  0x80
+#define KEY_LEFT_SHIFT			0x81
+#define KEY_LEFT_ALT			  0x82
+#define KEY_LEFT_GUI			  0x83
+#define KEY_RIGHT_CTRL			0x84
+#define KEY_RIGHT_SHIFT			0x85
+#define KEY_RIGHT_ALT			  0x86
+#define KEY_RIGHT_GUI			  0x87
+
+#define KEY_UP_ARROW			  0xDA
+#define KEY_DOWN_ARROW			0xD9
+#define KEY_LEFT_ARROW			0xD8
+#define KEY_RIGHT_ARROW			0xD7
+#define KEY_BACKSPACE			  0xB2
+#define KEY_TAB					    0xB3
+#define KEY_RETURN				  0xB0
+#define KEY_ESC					    0xB1
+#define KEY_INSERT				  0xD1
+#define KEY_DELETE				  0xD4
+#define KEY_PAGE_UP				  0xD3
+#define KEY_PAGE_DOWN			  0xD6
+#define KEY_HOME				    0xD2
+#define KEY_END					    0xD5	
+#define KEY_CAPS_LOCK			  0xC1
+
+#define KEY_F1					    0xC2
+#define KEY_F2					    0xC3
+#define KEY_F3					    0xC4
+#define KEY_F4					    0xC5
+#define KEY_F5					    0xC6
+#define KEY_F6					    0xC7
+#define KEY_F7					    0xC8
+#define KEY_F8					    0xC9
+#define KEY_F9					    0xCA
+#define KEY_F10					    0xCB
+#define KEY_F11					    0xCC
+#define KEY_F12					    0xCD
+
+#define KEY_F13					    0xF0	240
+#define KEY_F14					    0xF1	241
+#define KEY_F15					    0xF2	242
+#define KEY_F16					    0xF3	243
+#define KEY_F17					    0xF4	244
+#define KEY_F18					    0xF5	245
+#define KEY_F19					    0xF6	246
+#define KEY_F20					    0xF7	247
+#define KEY_F21					    0xF8	248
+#define KEY_F22					    0xF9	249
+#define KEY_F23					    0xFA	250
+#define KEY_F24					    0xFB	251
+
+#define KEY_MUTE				    0x7F
+#define KEY_VOLUMEUP			  0x80
+#define KEY_VOLUMEDOWN			0x81
+
+#define KEY_KEYPAD_0			  0x62
+#define KEY_KEYPAD_1			  0x59
+#define KEY_KEYPAD_2			  0x5A
+#define KEY_KEYPAD_3			  0x5B
+#define KEY_KEYPAD_4			  0x5C
+#define KEY_KEYPAD_5			  0x5D
+#define KEY_KEYPAD_6			  0x5E
+#define KEY_KEYPAD_7			  0x5F
+#define KEY_KEYPAD_8			  0x60
+#define KEY_KEYPAD_9			  0x61
+#define KEY_KEYPAD_DECIMAL	0x63
+#define KEY_KEYPAD_ENTER		0x58
+#define KEY_KEYPAD_PLUS			0x57
+#define KEY_KEYPAD_MINUS		0x56
+#define KEY_KEYPAD_MULTIPLY	0x55
+#define KEY_KEYPAD_DIVIDE		0x54
+#define KEY_KEYPAD_NUMLOCK	0x53
+#define KEY_KEYPAD_EQUALS		0x67
+
+Space is decimal 32, or 0x20 in Hexadecimal.
+Keyboard.write(' '); should work - as should Keyboard.write((char) 32); or Keyboard.write((char) 0x20);.
+*/
---
+++ RBLPControls.ino
@@ -0,0 +1,69 @@
+/* 
+ *  RECALBOX - LATTEPANDA (ARDUINO LEONARDO) PLAYER 1 CONTROLLER ENCODER WITH FRONT END CONTROLS
+ *  DERIVED FROM ROMBUSPINBALL https://github.com/circuitbeard/rombus-pinball
+ *  Borrowed heavily from Pimoronis Picade sketch at https://github.com/pimoroni/Picade-Sketch/tree/master/Picade
+ */
+
+#include "RBLPControls.h"
+#include "TimerOne.h"
+#include <Keyboard.h>
+
+#define DEBOUNCE_DELAY 25
+
+const uint8_t user_pins[] = { BTN_P1_START, BTN_P1_SELECT, BTN_P1_UP, BTN_P1_DOWN, BTN_P1_LEFT, BTN_P1_RIGHT, BTN_P1_A, BTN_P1_B, BTN_P1_X, BTN_P1_Y, BTN_P1_L1, BTN_P1_R1, BTN_FE_HOTHEY, BTN_FE_ACCEPT, BTN_FE_BACK, BTN_FE_MENU, BTN_FE_QUITEM };
+const uint8_t user_keys[] = { '1', '3', KEY_UP_ARROW, KEY_DOWN_ARROW, KEY_LEFT_ARROW, KEY_RIGHT_ARROW, 'z', KEY_LEFT_SHIFT, KEY_LEFT_ALT, KEY_LEFT_CTRL, ' ', 'x', 'c', 'a', 's', KEY_RETURN, KEY_ESC };
+
+static button_state button_states[BUTTON_COUNT] = {};
+
+void setup() {
+  Serial.begin(9600);
+  //while(!Serial);
+  Serial.setTimeout(100);
+
+  // Setup pins as input with pullup resistor
+  for ( uint8_t i = 0; i < BUTTON_COUNT; i++ ) {
+
+
+
+        pinMode(user_pins[i], INPUT_PULLUP);
+   }
+
+  Timer1.initialize(5000);
+  Timer1.attachInterrupt(update);
+  Timer1.start(); 
+}
+
+void update(void) {
+  for (int i = 0; i < BUTTON_COUNT; i++)
+  {
+    uint8_t state;
+    state = !digitalRead(user_pins[i]);
+    if (state != button_states[i].state && (millis() - button_states[i].last_change) > DEBOUNCE_DELAY) // has this input changed state since the last time we checked?
+    {
+      button_states[i].state = state; // update our state map so we know what's happening with this key in future
+      button_states[i].last_change = millis();
+      handle_key(i, state);
+    }
+  }
+}
+
+bool handle_key(uint8_t i, uint8_t state) {
+  uint8_t pin = user_pins[i];
+  uint8_t key = user_keys[i];
+  
+
+
+
+  
+  if (state) {
+    Keyboard.press(key);
+    digitalWrite(13, HIGH);
+  } else {
+    Keyboard.release(key);
+    digitalWrite(13, LOW);
+  }
+}
+
+void loop() {
+  // Do nothing...
+}
